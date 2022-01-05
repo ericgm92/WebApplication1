eff33d88c9ffa9fdb5f3d17edf1b5a665ebb6cce
ericgm92 <ericgamizmartinez1992@gmail.com>
2022-01-05 16:57:08 +0000

    Initial commit

---
+++ WebApplication1/Models/Gestores_Bd.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Gestores_Bd
+    {
+        [Key]
+        public int id { get; set; }
+        public string ciudad { get; set; }
+        public string pais { get; set; }
+        public int poblacion { get; set; }
+    }
+}
